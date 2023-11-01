874941137bbc3994edc6300c7e6f563b4dc59001
Philip Johnson <johnson@hawaii.edu>
2023-11-01 11:29:04 -1000

    Create SingleTextField.dart

---
+++ lib/dynamic-text-field/SingleTextField.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class SingleTextField extends StatefulWidget {
+  final String? initialValue;
+  final void Function(String) onChanged;
+
+  const SingleTextField({
+    super.key,
+    this.initialValue,
+    required this.onChanged,
+  });
+
+  @override
+  State createState() => _SingleTextFieldState();
+}
+
+class _SingleTextFieldState extends State<SingleTextField> {
+  late final TextEditingController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = TextEditingController();
+    _controller.text = widget.initialValue ?? '';
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: _controller,
+      onChanged: widget.onChanged,
+      decoration: const InputDecoration(hintText: "Enter your friend's name"),
+      validator: (v) {
+        if (v == null || v.trim().isEmpty) return 'Please enter something';
+        return null;
+      },
+    );
+  }
+}
